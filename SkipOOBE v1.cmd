1c64a06a01e6f9f692f4b60090c790e15aaed5fd
Ryder B. <153897299+Ryder-Blase@users.noreply.github.com>
2025-04-26 20:44:08 +0200

    Delete SkipOOBE v1.cmd

--- SkipOOBE v1.cmd
+++
@@ -1,5 +0,0 @@
-@echo off 
-reg delete HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OOBE /v LaunchUserOOBE /f
-net user /del defaultuser0
-net user Administrateur /active:yes
-shutdown /t 0 /r 
